578824abdc39e85394c8c5733dfeca8642d25663
Sebastian <92864754+sebhoron@users.noreply.github.com>
2024-10-11 13:16:46 +0100

    update homepage (#29)
    * update homepage
    
    * update components
    
    * fix img path

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,17 @@
-import {
-  createHashRouter,
-  createRoutesFromElements,
-  Outlet,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import "./App.css"
+import "./app.css"
 
 import { ThemeProvider } from "./context/ThemeContext";
 import { SpinnerAtom } from "./components/atoms/SpinnerAtom";
-import { Header } from "./components/organisms/Header/HeaderOrganism";
-import { HomePage } from "./components/pages/HomePage/HomePage";
-import { PortfolioPage } from "./components/pages/PortfolioPage";
-import { ErrorBoundary } from "./utils/ErrorBoundary";
-
-const router = createHashRouter(
-  createRoutesFromElements(
-    <Route element={<NavbarWrapper />} errorElement={<ErrorBoundary />}>
-      <Route index element={<HomePage />} />
-      <Route path="portfolio" element={<PortfolioPage />} />
-    </Route>
-  )
-);
-
-function NavbarWrapper() {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
-  )
-};
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <ThemeProvider>
       <RouterProvider
         fallbackElement={<SpinnerAtom />}
-        router={router}
+        router={Router}
       />
     </ThemeProvider>
   )
--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,47 +1,17 @@
-import { FaCode, FaPlay } from 'react-icons/fa';
+import { FaCode } from 'react-icons/fa';
 import { DiStreamline } from "react-icons/di";
 import { RiRobot2Line } from "react-icons/ri";
 import { IoIosCloudOutline } from "react-icons/io";
 import { v4 as uuid } from 'uuid';
-import { useEffect, useRef } from 'react';
 
-import './HomePage.css'
+import './home-page.css'
 
-import { Card } from '../../card/Card';
+import { Card } from '../../molecules/Card/Card';
 import { cards } from '../../../utils/Data';
-
-import Contact from '../../contact/Contact';
+import { Footer } from '../../organisms/Footer/Footer';
+import dev from '../../../assets/dev.png'
 
 export function HomePage() {
-  const skillRef = useRef(null!);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            animateProgressbar();
-            observer.disconnect();
-          }
-        });
-      },
-      { threshold: 0.5 }
-    );
-
-    observer.observe(skillRef.current);
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  const animateProgressbar = () => {
-    const progressBars = document.querySelectorAll<HTMLElement>('.skill__progress-line');
-
-    progressBars.forEach((progressBar) => {
-      const percent = progressBar.getAttribute('data-width');
-      progressBar.style.width = `${percent}%`;
-    });
-  };
   return (
     <div className='container home'>
       <section className='hero-section' id='home'>
@@ -54,22 +24,10 @@ export function HomePage() {
               Full Stack Developer <br /> Java, Python, TypeScript and React
             </h3>
           </div>
-          <a href='/resume.pdf' download='resume.pdf' className='button'>
-            Get Resume
-          </a>
-
-          <div className='video-link'>
-            <div className='link'>
-              <a href='https://www.youtube.com/watch?v=xvFZjo5PgG0'>
-                <FaPlay />
-              </a>
-            </div>
-            <span>Video Link</span>
-          </div>
         </div>
         <div className='hero-section__right'>
           <div className='hero-section__image'>
-            <img src='/homepage/dev.png' alt='hero image' />
+            <img src={dev} alt='hero image' />
             <div className='hero-section__image-half-round-shape'></div>
             <div className='social-links'>
               <a href=''>
@@ -103,35 +61,6 @@ export function HomePage() {
         </div>
       </section>
 
-      <section className='skill' id='skills' ref={skillRef}>
-        <div className='skill__left'>
-          <h2 className='section__title'>My Skills</h2>
-        </div>
-        <div className='skill__right'>
-          <div className='skill__wrapper'>
-            <div className='skill__tag'>Development</div>
-            <div className='skill__box'>
-              <div className='skill__progress-line' data-width='95'></div>
-              <div className='skill__percentage'>95%</div>
-            </div>
-          </div>
-          <div className='skill__wrapper'>
-            <div className='skill__tag'>AI Engineering</div>
-            <div className='skill__box'>
-              <div className='skill__progress-line' data-width='90'></div>
-              <div className='skill__percentage'>90%</div>
-            </div>
-          </div>
-          <div className='skill__wrapper'>
-            <div className='skill__tag'>Cloud</div>
-            <div className='skill__box'>
-              <div className='skill__progress-line' data-width='85'></div>
-              <div className='skill__percentage'>85%</div>
-            </div>
-          </div>
-        </div>
-      </section>
-
       <section className='portfolio' id='portfolio'>
         <h2 className='section__title'>My Portfolio</h2>
 
@@ -147,7 +76,7 @@ export function HomePage() {
       <div className='contact' id='contact'>
         <h2 className=' section__title'>Connect</h2>
 
-        <Contact />
+        <Footer />
       </div>
     </div>
   );
