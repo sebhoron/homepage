f7fd2f3bad17033896ffd567fe5d1533d064eade
sebhoron <92864754+sebhoron@users.noreply.github.com>
2024-10-11 10:10:28 +0100

    update homepage

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -3,7 +3,6 @@ import { DiStreamline } from "react-icons/di";
 import { RiRobot2Line } from "react-icons/ri";
 import { IoIosCloudOutline } from "react-icons/io";
 import { v4 as uuid } from 'uuid';
-import { useEffect, useRef } from 'react';
 
 import './HomePage.css'
 
@@ -13,35 +12,6 @@ import { cards } from '../../../utils/Data';
 import Contact from '../../contact/Contact';
 
 export function HomePage() {
-  const skillRef = useRef(null!);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            animateProgressbar();
-            observer.disconnect();
-          }
-        });
-      },
-      { threshold: 0.5 }
-    );
-
-    observer.observe(skillRef.current);
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  const animateProgressbar = () => {
-    const progressBars = document.querySelectorAll<HTMLElement>('.skill__progress-line');
-
-    progressBars.forEach((progressBar) => {
-      const percent = progressBar.getAttribute('data-width');
-      progressBar.style.width = `${percent}%`;
-    });
-  };
   return (
     <div className='container home'>
       <section className='hero-section' id='home'>
@@ -54,10 +24,6 @@ export function HomePage() {
               Full Stack Developer <br /> Java, Python, TypeScript and React
             </h3>
           </div>
-          <a href='/resume.pdf' download='resume.pdf' className='button'>
-            Get Resume
-          </a>
-
           <div className='video-link'>
             <div className='link'>
               <a href='https://www.youtube.com/watch?v=xvFZjo5PgG0'>
@@ -100,35 +66,6 @@ export function HomePage() {
               features={card?.features}
             />
           ))}
-        </div>
-      </section>
-
-      <section className='skill' id='skills' ref={skillRef}>
-        <div className='skill__left'>
-          <h2 className='section__title'>My Skills</h2>
-        </div>
-        <div className='skill__right'>
-          <div className='skill__wrapper'>
-            <div className='skill__tag'>Development</div>
-            <div className='skill__box'>
-              <div className='skill__progress-line' data-width='95'></div>
-              <div className='skill__percentage'>95%</div>
-            </div>
-          </div>
-          <div className='skill__wrapper'>
-            <div className='skill__tag'>AI Engineering</div>
-            <div className='skill__box'>
-              <div className='skill__progress-line' data-width='90'></div>
-              <div className='skill__percentage'>90%</div>
-            </div>
-          </div>
-          <div className='skill__wrapper'>
-            <div className='skill__tag'>Cloud</div>
-            <div className='skill__box'>
-              <div className='skill__progress-line' data-width='85'></div>
-              <div className='skill__percentage'>85%</div>
-            </div>
-          </div>
         </div>
       </section>
 
