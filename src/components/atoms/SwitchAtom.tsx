df78c1a374a356a833df60dafd14e7dfe8eb7da4
Sebastian <92864754+sebhoron@users.noreply.github.com>
2024-10-28 15:38:04 +0000

    fixed switch (#30)

--- src/components/atoms/SwitchAtom.tsx
+++ src/components/atoms/SwitchAtom.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import Switch from "react-switch";
 import { PiMoonStarsFill } from "react-icons/pi";
 import { HiOutlineSun } from "react-icons/hi2";
+
 import { ThemeContext } from "../../context/ThemeContext";
 
 export const SwitchAtom: React.FC = () => {
@@ -16,8 +17,18 @@ export const SwitchAtom: React.FC = () => {
       onColor="#86d3ff"
       onHandleColor="#2693e6"
       handleDiameter={30}
-      uncheckedIcon={<HiOutlineSun />}
-      checkedIcon={<PiMoonStarsFill />}
+      uncheckedIcon={false}
+      checkedIcon={false}
+      uncheckedHandleIcon={
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+          <HiOutlineSun style={{ height: "70%", width: "70%" }} />
+        </div>
+      }
+      checkedHandleIcon={
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+          <PiMoonStarsFill style={{ height: "70%", width: "70%" }} />
+        </div>
+      }
       boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
       activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
       height={20}
